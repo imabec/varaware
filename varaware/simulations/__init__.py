31beae00656dc76b7b3dfb3bb4372a5c76a4f961
imabec <52859459+imabec@users.noreply.github.com>
2026-05-18 13:38:44 -0700

    Update __init__.py

--- varaware/simulations/__init__.py
+++ varaware/simulations/__init__.py
@@ -1,3 +1,4 @@
 from .simulate_scvc import SCSVSimulator
+from .simulate_scvc import simulate_scvc
 from .simulation import simulate_variance_aware
-__all__ = ["SCSVSimulator","simulate_variance_aware"]
+__all__ = ["SCSVSimulator","simulate_variance_aware","simulate_scvc"]
